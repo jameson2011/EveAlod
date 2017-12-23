8ac30bda5446a8f000a0eccfc6f57aba9b130bff
jameson2011 <jameson2011@users.noreply.github.com>
2017-12-23 16:49:10 +0000

    Web

--- src/EveAlod.Data/Library.fs
+++
@@ -1,5 +0,0 @@
-namespace EveAlod.Data
-
-module Say =
-    let hello name =
-        printfn "Hello %s" name
---
+++ src/EveAlod.Data/Web.fs
@@ -0,0 +1,47 @@
+﻿namespace EveALOD.Data
+    module Web=
+
+        open System.IO
+        open System.Net
+    
+        let private userAgent = "Maintainer:" // TODOTK: make configurable
+            
+        let private getData (url: string) =
+            async {
+                try
+                    let req = HttpWebRequest.Create(url) :?> HttpWebRequest
+                    req.UserAgent <- userAgent 
+                    let! resp = req.AsyncGetResponse()
+                    use stream = resp.GetResponseStream()
+                    let rdr = new StreamReader(stream)
+                    return (rdr.ReadToEnd())
+                with _ -> return ""
+            }
+
+        let private getEsiData (uri) (id: string) =        
+            id
+            |> sprintf uri 
+            |> getData
+
+        
+        let getKm (url: string) =
+            getData url
+            
+
+        
+        let sendDiscord (channelId: string) (token: string) (content: string)=
+            async {
+                // TODOTK: error handling
+                // post to this...
+                let url = sprintf "https://discordapp.com/api/webhooks/%s/%s" channelId token
+                                
+                use client = new System.Net.Http.HttpClient()
+                // TODOTK: client.DefaultRequestHeaders.Add("User-Agent", userAgent)
+        
+                let values = new System.Collections.Generic.Dictionary<string, string>()
+                values.Add("content", content)
+                let content = new System.Net.Http.FormUrlEncodedContent(values)
+
+                let! response = client.PostAsync(url, content) |> Async.AwaitTask
+                response |> ignore
+            }
