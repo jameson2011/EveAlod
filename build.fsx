b30fe8d8ad47d385fab7c3b9436ed21d316bcd02
jameson2011 <jameson2011@users.noreply.github.com>
2018-02-10 18:48:51 +0000

    remove kill serialisation & fsPickler as redundant; add integration test shell

--- build.fsx
+++ build.fsx
@@ -17,30 +17,43 @@ Target "ScrubArtifacts" (fun _ -> CleanDirs [ buildDir;
 Target "BuildApp" (fun _ -> 
                             !! "src/**/*.fsproj"
                             -- "src/**/*.Tests.fsproj"
+                            -- "src/**/*.IntegrationTests.fsproj"
                             |> MSBuildRelease buildDir "Build"
                             |> Log "AppBuild-Output: ")
 
 Target "LintApp" (fun _ ->
                             !! "src/**/*.fsproj"
                             -- "src/**/*.Tests.fsproj"
+                            -- "src/**/*.IntegrationTests.fsproj"
                             |> Seq.iter (FSharpLint 
                                             (fun o -> { o with FailBuildIfAnyWarnings = true }))
                 )
+
 Target "BuildTests" (fun _ -> 
                             !! "src/**/*.Tests.fsproj"
+                            ++ "src/**/*.IntegrationTests.fsproj"
                             |> MSBuildRelease buildTestsDir "Build"
-                            |> Log "TestsBuild-Output: ")
+                            |> Log "BuildTests-Output: ")
 
 Target "RunUnitTests" (fun _ -> 
                             !! (buildTestsDir @@ "*.Tests.dll")
                             |> xUnit2 (fun p ->
                                             { p with 
                                                 ShadowCopy = false;
-                                                HtmlOutputPath = Some (buildTestsDir @@ "TestResults.html")
+                                                HtmlOutputPath = Some (buildTestsDir @@ "UnitTestResults.html")
                                             }
                                         )                            
                             )
-                            
+
+Target "RunIntegrationTests" (fun _ -> 
+                            !! (buildTestsDir @@ "*.IntegrationTests.dll")
+                            |> xUnit2 (fun p ->
+                                            { p with 
+                                                ShadowCopy = false;
+                                                HtmlOutputPath = Some (buildTestsDir @@ "IntegrationTestResults.html")
+                                            }
+                                        )                            
+                            )
 
 Target "Default" (fun _ -> trace "Done!" )
 
@@ -56,6 +69,7 @@ Target "Default" (fun _ -> trace "Done!" )
 ==> "RunUnitTests"
 ==> "Default"
 
-
+"BuildTests"
+==> "RunIntegrationTests"
 
 RunTargetOrDefault "Default"
---
+++ src/EveAlod.Services.IntegrationTests/StaticDataProviderTests.fs
@@ -0,0 +1,12 @@
+﻿namespace EveAlod.Services.IntegrationTests
+
+open Xunit
+open FsCheck
+open FsCheck.Xunit
+open EveAlod.Data
+
+module StaticDataProviderTests=
+    
+    [<Fact>]
+    let ``Test``() =
+        ignore 0
