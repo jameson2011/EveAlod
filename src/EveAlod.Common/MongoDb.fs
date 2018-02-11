caba70e12255b6a1c05d99243ddb37257776f24b
jameson2011 <jameson2011@users.noreply.github.com>
2018-02-11 16:43:17 +0000

    cleanup

--- src/EveAlod.Common/MongoDb.fs
+++ src/EveAlod.Common/MongoDb.fs
@@ -9,31 +9,35 @@ module MongoDb=
     let connectionString server = 
         sprintf "mongodb://%s:27017" server            
         
+    let pingDb (db: IMongoDatabase) = 
+        new MongoDB.Driver.BsonDocumentCommand<Object>(BsonDocument.Parse("{ping:1}"))
+                    |> db.RunCommand 
+                    |> ignore
+        db
+
     let initDb dbName (connection: string) =            
         let client= MongoClient(connection)                                            
-        let db = client.GetDatabase(dbName)
-                
-        new MongoDB.Driver.BsonDocumentCommand<Object>(BsonDocument.Parse("{ping:1}"))
-                    |> db.RunCommand |> ignore
-        db
+        let db = client.GetDatabase(dbName)                        
+        db |> pingDb
+        
 
     let setIndex (path: string) (collection: IMongoCollection<'a>) =                        
         let json = sprintf "{'%s': 1 }" path
         let def = IndexKeysDefinition<'a>.op_Implicit(json)
             
-        let r = collection.Indexes.CreateOne(def)
+        collection.Indexes.CreateOne(def) |> ignore
 
         collection
         
     let getCollection colName (db: IMongoDatabase) =
         db.GetCollection(colName)                
                             
-    let defaultCollection server dbName collectionName =
+    let initCollection server dbName collectionName =
         server
         |> connectionString
         |> initDb dbName
         |> getCollection collectionName
-        |> setIndex "_v.package.killID"
+        
 
 module Bson =
     open MongoDB.Bson.IO
