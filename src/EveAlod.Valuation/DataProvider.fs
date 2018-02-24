96e101521d8782e4cffe8af1232442049c831605
jameson2011 <jameson2011@users.noreply.github.com>
2018-02-24 19:04:35 +0000

    cleanup

--- src/EveAlod.Valuation/DataProvider.fs
+++ src/EveAlod.Valuation/DataProvider.fs
@@ -2,13 +2,31 @@
 
 open System
 open EveAlod.Common
+open EveAlod.Common.Json
 open EveAlod.Common.Web
+
+type private KillmailHistoryIdProvider = FSharp.Data.JsonProvider<"./SampleKillHistory.json">
 
 type DataProvider()=
 
     let getData = httpClient() |> getData
+    let uriDate (date: DateTime) = date.ToString("yyyyMMdd")        
+    let historyUri = sprintf "https://zkillboard.com/api/history/%s/"
     let shipStatsUri = sprintf "https://zkillboard.com/api/stats/shipTypeID/%s/"
     let statsAge = 12
+
+    
+    let getDayKillmailIds (date) =        
+        async {            
+            let! resp = date |> uriDate |> historyUri |> getData 
+            let root = match resp.Status with
+                        | HttpStatus.OK -> KillmailHistoryIdProvider.Parse(resp.Message)
+                        | s -> s.ToString() |> failwith // TODO:
+            // TODO:
+            let ids = root.JsonValue
+
+            return []
+        }
 
     member __.ShipStatistics(shipTypeId: string)=
         async {
@@ -19,3 +37,4 @@ type DataProvider()=
                     | _ ->              None                    
         }
 
+    member __.KillIds(date: DateTime)= getDayKillmailIds date
