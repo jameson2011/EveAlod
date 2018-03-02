94eca3b90f66b58740d97eefbd7e1fc38c0cda49
jameson2011 <jameson2011@users.noreply.github.com>
2018-03-02 14:38:22 +0000

    Optimise rehydration

--- src/EveAlod.Valuation/RehydrateStatsActorfs.fs
+++ src/EveAlod.Valuation/RehydrateStatsActorfs.fs
@@ -8,12 +8,6 @@ open EveAlod.Common.Strings
 open EveAlod.Common.MongoDb
 open EveAlod.Data
 
-type private RehydrateStatsState = 
-    {
-        Start: DateTime
-        TotalRecords: int64        
-    } with 
-    static member Empty = { Start = DateTime.MinValue; TotalRecords = 0L; }
 
 type RehydrateStatsActor(log: PostMessage, sendShipStats: PostValuationMessage, config: ValuationConfiguration)=
 
@@ -64,85 +58,75 @@ type RehydrateStatsActor(log: PostMessage, sendShipStats: PostValuationMessage, 
                     Total = parseValues "total" elements seed.Total } 
             |> parseHeader elements
         
-        
+    let docsMap (cursor: IAsyncCursor<BsonDocument>) =
+        seq {
+            while cursor.MoveNext() do
+                let c = cursor.Current
+                yield c
+        }
 
-    let getFirstCachedItem()=
+    let getCachedStats()=
             async {                
                 try
                     use! docs = shipTypeStatsCollection.Value.FindAsync(fun d -> true) |> Async.AwaitTask
-                
-                    let! doc = docs.FirstOrDefaultAsync() |> Async.AwaitTask
-                    return match doc with
-                            | null ->   Choice1Of3 None
-                            | _ ->      let objectId = Bson.getObjectId doc
-                                        Choice2Of3 (objectId, (toStatsEntry doc))                
+
+                    let result = docs 
+                                    |> docsMap |> Seq.collect (fun ds -> ds)
+                                    |> Seq.map toStatsEntry
+                                    |> List.ofSeq
+                    return Choice1Of2 result
+                    
                 with
                 | e ->  logException e
-                        return Choice3Of3 e
+                        return Choice2Of2 e
             }
-    
-    let getNextCachedItem(id: ObjectId) =
-            async{
-                try
-                    let filterJson = sprintf @"{_id: { $gt: ObjectId(""%s"") } }" (id.ToString())
-                    let idFieldFilter = new JsonFilterDefinition<BsonDocument>(filterJson)
-                                    
-                    use! docs = shipTypeStatsCollection.Value.FindAsync(idFieldFilter) |> Async.AwaitTask
-                
-                    let! doc = docs.FirstOrDefaultAsync() |> Async.AwaitTask
-                
-                    return match doc with
-                            | null ->   Choice1Of3 None
-                            | d ->      let objectId = Bson.getObjectId doc
-                                        Choice2Of3 (objectId, (toStatsEntry doc))
-                with
-                | e ->  logException e
-                        return Choice3Of3 e
-            }
-       
-    let onGetNext(id: ObjectId option)=
+        
+        
+    let onStart()=
         async {
-                
-            let! r = match id with
-                        | None -> getFirstCachedItem()
-                        | Some id -> getNextCachedItem(id)                        
-            
-            return match r with
-                    | Choice2Of3 (id, stats) -> 
-                            sprintf "Got %s:%s" stats.ShipTypeId (stats.Period.ToShortDateString()) |> logTrace
-                            stats |> ValuationActorMessage.ImportShipTypePeriod |> sendShipStats                        
-                            Choice1Of3 id
-                    | Choice1Of3 _ -> Choice2Of3 None
-                    | Choice3Of3 e -> Choice3Of3 e
+            try
+                let start = DateTime.UtcNow
+                logInfo "Reading stats cache..." 
+                let! items = getCachedStats()
+                match items with
+                | Choice1Of2 stats ->                                 
+                                logInfo "Starting stats merge..."
+                                let count = stats 
+                                            // No need to wait! The actor queues have single threaded consumers 
+                                            // so any subsequent outside request will fall to the back of the queue
+                                            |> Seq.map (ValuationActorMessage.ImportShipTypePeriod >> sendShipStats)
+                                            |> Seq.length
+
+                                return Choice1Of2 (DateTime.UtcNow - start, count)
+                            
+                | Choice2Of2 e -> return (Choice2Of2 e)
+            with
+            | e ->  logException e
+                    return Choice2Of2 e
         }
-
-    let pipe = MailboxProcessor<ObjectId option * AsyncReplyChannel<bool>>.Start(fun inbox ->
-        let rec loop (state: RehydrateStatsState) = 
+        
+    let pipe = MailboxProcessor<AsyncReplyChannel<bool>>.Start(fun inbox ->
+        let rec loop () = 
             async {                
-                let! lastId,ch = inbox.Receive()
+                let! ch = inbox.Receive()
 
-                let! nextId = onGetNext(lastId)
-                
-                match nextId with
-                | Choice1Of3 id -> 
-                        (Some id, ch) |> inbox.Post
-                        return! loop {state with TotalRecords = state.TotalRecords + 1L }
-                | Choice2Of3 _ -> 
-                        let duration = DateTime.UtcNow - state.Start
-                        "Rehydration complete." |> logInfo 
-                        sprintf "Rehydrate Duration: %s Total Records: %i " (duration.ToString()) state.TotalRecords |> logInfo
-                        ch.Reply true
-                        return (ignore 0)
-                | Choice3Of3 e -> 
-                        "Rehydration failing." |> logError
-                        ch.Reply false
-                        return (ignore 0)
+                let! result = onStart()
+                match result with
+                | Choice1Of2 (duration,count) ->                     
+                    "Rehydration complete." |> logInfo 
+                    sprintf "Rehydrate Duration: %s Total Records: %i " (duration.ToString()) count |> logInfo
+                    ch.Reply true
+                    return (ignore 0)
+                | Choice2Of2 e ->                      
+                    "Rehydration finished with error." |> logError
+                    ch.Reply false
+                    return (ignore 0)                
             }            
-        loop( { RehydrateStatsState.Empty with Start = DateTime.UtcNow } )
+        loop( )
         )
 
     do pipe.Error.Add(logException)
     
     member __.Start() =
         "Starting Rehydrate..." |> logInfo
-        pipe.PostAndAsyncReply (fun ch -> None, ch)
+        pipe.PostAndAsyncReply (fun ch -> ch)
