5006921cf13932a9c21fea80c9c2351b5216ea0f
jameson2011 <jameson2011@users.noreply.github.com>
2018-03-03 10:40:11 +0000

    config validation & reporting

--- src/EveAlod.Services/Config.fs
+++ src/EveAlod.Services/Config.fs
@@ -27,13 +27,22 @@
 
         let validateKillSource (config: Configuration) =
             seq{
-                if isNullWhitespace config.KillSourceUri then
-                    yield "Missing or empty killSourceUri in configuration."
+                let ok,_  = System.Uri.TryCreate(config.KillSourceUri, UriKind.Absolute)
+                if not ok then
+                    yield "Missing or invalid killSourceUri in configuration."
             }
+
+        let validateValuationUri (config: Configuration)=
+            seq {
+                    let ok,_ = System.Uri.TryCreate(config.KillValuationUri, UriKind.Absolute)
+                    if not ok then
+                        yield "Missing or invalid killValuationUri in configuration."
+                }
 
         let validate config = 
             let msg = seq { 
                                 yield! validateKillSource config;
+                                yield! validateValuationUri config;
                                 yield! validateDiscordId config;
                                 yield! validateDiscordToken config;
                                 yield! validateCorpId config;
@@ -45,12 +54,13 @@
 
         let report (log: PostMessage) (config: Configuration) =
             [ 
-                sprintf "Kills source:  %s" config.KillSourceUri;
-                sprintf "Minimum score: %f" config.MinimumScore;
-                sprintf "Corp ticker:   %s" config.CorpTicker;
-                sprintf "Corp ID:       %s" config.CorpId;
-                sprintf "Discord:       %s" config.ChannelId;
-                sprintf "Discord token: %s" config.ChannelToken;
+                sprintf "Kills source:   %s" config.KillSourceUri;
+                sprintf "Kill valuation: %s" config.KillValuationUri;
+                sprintf "Minimum score:  %f" config.MinimumScore;
+                sprintf "Corp ticker:    %s" config.CorpTicker;
+                sprintf "Corp ID:        %s" config.CorpId;
+                sprintf "Discord:        %s" config.ChannelId;
+                sprintf "Discord token:  %s" config.ChannelToken;
             ]            
             |> Seq.map ActorMessage.Info
             |> Seq.iter log
