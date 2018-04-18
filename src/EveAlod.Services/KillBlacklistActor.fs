ece099efe358315560b34583754a3cdf7ea45d3e
jameson2011 <jameson2011@users.noreply.github.com>
2018-04-18 22:39:49 +0100

    remove dupe filtering as handled elsewhere

--- src/EveAlod.Services/KillBlacklistActor.fs
+++ src/EveAlod.Services/KillBlacklistActor.fs
@@ -24,31 +24,25 @@
             
 
         let pipe = MessageInbox.Start(fun inbox -> 
-            let rec getNext(buffer: MutableCappedBuffer<string>) = async {
+            let rec getNext() = async {
                 
                 let! msg = inbox.Receive()
 
-                let newBuffer = try
-                                    match msg with                                    
-                                    | Killmail km ->    
-                                                if isBlacklisted km then
-                                                    km.Id |> sprintf "Kill %s ignored as blacklisted" |> logTrace
-                                                    buffer
-                                                else if buffer.Contains(km.Id) then
-                                                    km.Id |> sprintf "Kill %s ignored as a duplicate." |> logTrace
-                                                    buffer
-                                                else                                     
-                                                    forward km
-                                                    buffer.Add(km.Id)
-                                    | _ ->      buffer
+                try
+                    match msg with                                    
+                    | Killmail km ->    
+                                if isBlacklisted km then
+                                    km.Id |> sprintf "Kill %s ignored as blacklisted" |> logTrace
+                                else                                     
+                                    forward km
+                    | _ ->      ignore 0
                                     
-                                with e ->   logException e
-                                            buffer
+                with e ->   logException e                                            
                 
-                return! getNext(newBuffer)
+                return! getNext()
                 }
         
-            getNext(new MutableCappedBuffer<string>(10))
+            getNext()
         )
         
         do pipe.Error.Add(logException)
