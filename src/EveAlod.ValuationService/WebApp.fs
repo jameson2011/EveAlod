5bdafcdf2219dea4682125480fd5a18455722984
Jameson2011 <j7db@hotmail.co.uk>
2019-04-14 17:05:54 +0100

    log web service requests

--- src/EveAlod.ValuationService/WebApp.fs
+++ src/EveAlod.ValuationService/WebApp.fs
@@ -16,25 +16,27 @@ module WebApp=
 
 
     let webRoutes (logger: PostMessage) (ships: ShipStatsActor)= 
+        let logRequest = WebServices.logRouteInvoke logger
         choose
             [   GET  >=> choose [                                    
-                                    pathScan "/stats/%s/" (WebServices.getShipTypeStatsJson ships)
+                                    pathScan "/stats/%s/" (fun id -> logRequest >=> WebServices.getShipTypeStatsJson ships id)
                                                             >=> WebServices.setNoCache >=> WebServices.jsonMimeType
 
-                                    path "/stats/" >=> WebServices.getShipSummaryStatsJson ships
-                                                            >=> WebServices.setNoCache >=> WebServices.jsonMimeType                                    
+                                    path "/stats/" >=> logRequest 
+                                                    >=> WebServices.getShipSummaryStatsJson ships
+                                                    >=> WebServices.setNoCache >=> WebServices.jsonMimeType                                    
 
-                                    pathScan "/valuation/%s/%f/" (fun (id, total) -> WebServices.getShipTypeValuation ships id None total)
+                                    pathScan "/valuation/%s/%f/" (fun (id, total) -> logRequest >=> WebServices.getShipTypeValuation ships id None total)
                                                             >=> WebServices.setNoCache >=> WebServices.jsonMimeType
 
-                                    pathScan "/valuation/%s/%f/%f/" (fun (id, total, fitted) -> WebServices.getShipTypeValuation ships id (Some fitted) total)
+                                    pathScan "/valuation/%s/%f/%f/" (fun (id, total, fitted) -> logRequest >=> WebServices.getShipTypeValuation ships id (Some fitted) total)
                                                             >=> WebServices.setNoCache >=> WebServices.jsonMimeType
 
                                     path "/favicon.ico" >=> Suave.Successful.no_content >=> WebServices.setCacheLimit 99999999
                                 ];
 
                 POST >=> choose [
-                                    path "/kill/" >=> WebServices.postKill logger ships >=> WebServices.jsonMimeType
+                                    path "/kill/" >=> logRequest >=> WebServices.postKill logger ships >=> WebServices.jsonMimeType
                                 ]
             ]
 
